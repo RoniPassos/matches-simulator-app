9ef46b3e308fc0351b6e8c70f03ffefadc06807f
Roni <roni.gabriel@hotmail.com>
2022-06-14 11:21:08 -0300

    Desenvolvimento de Layouts de listagem e detalhes das simulaçoes do jogos

---
+++ app/src/main/java/com/example/meuprimeiroapp/DetailActivity.kt
@@ -0,0 +1,22 @@
+package com.example.meuprimeiroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.meuprimeiroapp.databinding.ActivityDetailBinding
+
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
